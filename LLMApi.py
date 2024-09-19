ffb027d1512f614c60b4783aa73ae4122062197f
Spencer Namazinia <sohrab.namazinia@gmail.com>
2024-09-18 21:34:04 -0400

    update llm calls - discrete default

--- LLMApi.py
+++ LLMApi.py
@@ -11,25 +11,33 @@ class LLMApi:
     DIVERSITY = "diversity"
     prompt_relevance = "The following query and review are about an item. Estimate the relevance of the query and the review as a floating point number in a scale of 0.0 to 1.0:\Query: {query}\nReview: {d}"
     prompt_diversity = "The following two comments are about an item. Estimate the diversity of these two comments as a floating point number in a scale of 0.0 to 1.0:\nComment 1: {d1}\nComment 2: {d2}"
-    def __init__(self) -> None:
+    # NOTE: set this false if not needed
+    def __init__(self, is_output_discrete=True) -> None:
         self.model_name = "gpt-4-turbo"
         self.model = ChatOpenAI(model=self.model_name).with_structured_output(Rating)
         self.prompt_relevance = ChatPromptTemplate.from_template(LLMApi.prompt_relevance)
         self.prompt_diversity = ChatPromptTemplate.from_template(LLMApi.prompt_diversity)
+        self.is_output_discrete = is_output_discrete
         
     
     def call_llm_relevance(self, query, d):
         """Estimate the relevance of the input to the query as a float number in the scale of 0.0 to 1.0"""
         chain = self.prompt_relevance | self.model 
         result = chain.invoke({"query": query, "d": d})
-        return result.rate
+        if not self.is_output_discrete:
+            return result.rate
+        else:
+            return round(result.rate, 1)
     
     
     def call_llm_diversity(self, d1, d2):
         """Estimate how diverse the two inputs are as a float number in the scale of 0.0 to 1.0"""
         chain = self.prompt_diversity | self.model 
         result = chain.invoke({"d1": d1, "d2": d2})
-        return result.rate
+        if not self.is_output_discrete:
+            return result.rate
+        else:
+            return round(result.rate, 1)
 
 # api = LLMApi()
 # result = api.call_llm_relevance("I need a camera which can zoom very much", "this camera was good, it could magnify very well")
--- Ranking.py
+++ Ranking.py
@@ -4,16 +4,8 @@ import time
 from LLMApi import LLMApi
 from copy import deepcopy
 import math
+from utilities import * 
 
-class TopKResult:
-    def __init__(self, algorithm, candidates_set, time, api_calls, entropy, entropydep) -> None:
-        self.algorithm = algorithm
-        self.candidates_set = candidates_set
-        self.time = time
-        self.api_calls = api_calls
-        self.entropy = entropy
-        self.entropydep = entropydep
-    
 class Metric:
     def __init__(self, name: str, n: int, m: int):
         self.name = name
@@ -55,56 +47,6 @@ class Metric:
 
     def __str__(self):
         return f"Table(name={self.name}, shape=({self.n}, {self.m}))\n{self.table}"
-
-def read_documents(input_file=None, n=4, mock_llms=False):
-    if mock_llms:
-        return [""] * n
-    with open(input_file, 'r') as file:
-        result = file.read().splitlines()
-    return result
-
-def init_candidates_set(n, k, lb_init_value, ub_init_value):
-    combinations = itertools.combinations(range(n), k)
-    candidates_set = {(combination): (lb_init_value, ub_init_value) for combination in combinations}
-    return candidates_set
-
-def check_pair_exist(candidate, pair):
-    return (pair[0] in candidate and pair[1] in candidate)
-
-def choose_2(k):
-    if k < 2:
-        return 0  
-    return k * (k - 1) // 2
-
-def compute_exact_scores_baseline(metrics, candidates_set):
-    relevance_table = metrics[0].table
-    diversity_table = metrics[1].table
-    k = len(next(iter(candidates_set)))
-    result = {}
-
-    for candidate, _ in candidates_set.items():
-        relevance_scores = [relevance_table[0, doc] for doc in candidate]
-        relevance_score = sum(relevance_scores) / k
-        
-        diversity_scores = 0
-        candidate_list = list(candidate)
-        for i in range(k):
-            for j in range(i + 1, k):
-                diversity_scores += (diversity_table[candidate_list[i], candidate_list[j]])
-        diversity_score = diversity_scores / choose_2(k)
-        
-        total_score = relevance_score + diversity_score
-        
-        result[candidate] = total_score
-    
-    return result
-
-def check_prune(tuple_1, tuple_2):
-    candidate_1, bounds_1 = tuple_1[0], tuple_1[1]
-    candidate_2, bounds_2 = tuple_2[0], tuple_2[1]
-    if bounds_1[0] >= bounds_2[1]: return candidate_2
-    if bounds_2[0] >= bounds_1[1]: return candidate_1
-    return None  
 
 def call_all_llms_relevance(input_query, documents, relevance_table, candidates_set, k, mocked_table = None):
     # relevance 
@@ -541,7 +483,7 @@ def prune(candidates_set, updated_keys):
             candidates_set.pop(key)
     return candidates_set
 
-def find_top_k(input_query, documents, k, metrics, methods, mock_llms = False):
+def find_top_k(input_query, documents, k, metrics, methods, mock_llms = False, is_output_discrete=True):
     results = []
     mocked_tables = None
 
@@ -605,6 +547,7 @@ methods = [MIN_UNCERTAINTY, LOWEST_OVERLAP, EXACT_BASELINE, NAIVE]
 #methods = [MIN_UNCERTAINTY]
 #methods = ["Exact_Baseline", "Naive"]
 mock_llms = True
+
 
 # run
 documents = read_documents(input_path, n, mock_llms)
