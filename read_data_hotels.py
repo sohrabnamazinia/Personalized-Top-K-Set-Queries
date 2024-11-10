dbcbbc328555a179d6c06d5eb4d1cf93e77eedce
Spencer Namazinia <sohrab.namazinia@gmail.com>
2024-11-10 13:22:24 -0500

    fix read_data

--- read_data_hotels.py
+++ read_data_hotels.py
@@ -1,16 +1,40 @@
 import pandas as pd 
 
+# def read_data(input_file="hotels.csv", n=1000):
+#     nRowsRead = n 
+#     input_path = "dataset_hotels/" + input_file
+#     df = pd.read_csv(input_path, delimiter=',', nrows=nRowsRead, encoding='ISO-8859-1')
+#     df.columns = df.columns.str.strip()
+    
+#     df_filtered = df[df['Description'].notnull()]
+#     grouped = df_filtered.groupby('HotelName')['Description'].apply(list)
+#     descriptions = grouped.to_dict()
+    
+#     return descriptions
+
 def read_data(input_file="hotels.csv", n=1000):
-    nRowsRead = n 
     input_path = "dataset_hotels/" + input_file
-    df = pd.read_csv(input_path, delimiter=',', nrows=nRowsRead, encoding='ISO-8859-1')
+    df = pd.read_csv(input_path, delimiter=',', encoding='ISO-8859-1')
     df.columns = df.columns.str.strip()
     
     df_filtered = df[df['Description'].notnull()]
-    grouped = df_filtered.groupby('HotelName')['Description'].apply(list)
+    
+    unique_hotels = set()
+    indices = []
+    for index, row in df_filtered.iterrows():
+        hotel_name = row['HotelName']
+        if hotel_name not in unique_hotels:
+            unique_hotels.add(hotel_name)
+        indices.append(index)
+        if len(unique_hotels) >= n:
+            break
+    
+    df_limited = df_filtered.loc[indices]
+    grouped = df_limited.groupby('HotelName')['Description'].apply(list)
     descriptions = grouped.to_dict()
     
     return descriptions
+
 
 def merge_descriptions(hotels_descriptions):
     merged_descriptions = []
