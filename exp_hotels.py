d40baf949acee30dc099be032ddaa428729e2a6b
Spencer Namazi Nia <sn773@login02.tartan.njit.edu>
2024-11-25 18:26:13 -0500

    push new results

--- exp_hotels.py
+++ exp_hotels.py
@@ -5,7 +5,7 @@ from utilities import RELEVANCE, DIVERSITY, NAIVE, MAX_PROB, EXACT_BASELINE, get
 
 # List of (n, k) tuples for experimentation
 # experiments = [(15, 2), (64, 2)]  
-experiments = [(32, 2), (90, 2)] 
+experiments = [(50, 2), (50, 4), (50, 6), (50, 8), (50, 10)] 
 dataset_name = "hotels"
 input_query = "Affordable hotel"
 # relevance_definition = "Rating_of_the_hotel"
@@ -14,7 +14,7 @@ relevance_definition = "Distance_from_city_center"
 diversity_definition = "Star_rating"
 metrics = [RELEVANCE, DIVERSITY]
 use_MGTs = True
-use_filtered_init_candidates = False
+use_filtered_init_candidates = True
 report_entropy_in_naive = False
 methods = [MAX_PROB, NAIVE, EXACT_BASELINE]  
 output_name = "Results_Hotels_REL_" + relevance_definition + "_DIV_" + diversity_definition
--- generate_filtered_init_candidates.py
+++ generate_filtered_init_candidates.py
@@ -2,7 +2,7 @@ from Ranking import store_all_top_k_candidates, store_top_k_candidates
 
 n_values = [50, 40, 30, 20, 20]
 k_values = [2, 4, 6, 8, 10]
-number_of_final_candidates = 100
+number_of_final_candidates = 500
 random_selection = True
 
 store_all_top_k_candidates(n_values, k_values, number_of_final_candidates, radnom_selection=random_selection)
